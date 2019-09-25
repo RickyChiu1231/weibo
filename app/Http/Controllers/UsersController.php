daf312b9dd5df0372e7bb9c34c166cc587a05300
RickyChiu1231 <laiyin101020@gmail.com>
2019-09-25 10:04:33 +0000

    registrationFunction

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,10 +20,19 @@ class UsersController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'name' => 'required|max:50',
+            'name' => 'required|min:4|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('warning', 'Welcome to our webiste');
+
+        return redirect()->route('users.show', [$user]);
     }
 }
